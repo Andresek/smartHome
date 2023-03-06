97df89f028099268f79cf803b915dc047d60e906
Andrew S <samum_94@mail.ru>
2023-03-06 12:38:20 +0300

    Turning on radio stations

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -6,6 +6,20 @@ public class Radio {
         return currentRadio;
     }
 
+    public void next() {
+        if (currentRadio == 9) {
+            currentRadio = 0;
+        }
+        else currentRadio = currentRadio + 1;
+    }
+
+    public void prev() {
+        if (currentRadio == 0) {
+            currentRadio = 9;
+        }
+        else currentRadio = currentRadio - 1;
+    }
+
     public void setCurrentRadio(int newCurrentRadio) {
         if (newCurrentRadio < 0) {
             return;
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -30,7 +30,7 @@ class RadioTest {
     }
 
     @Test
-    void setCurrentRadioOverLimit() {
+    void setCurrentRadioOverNine() {
         Radio station = new Radio();
 
         station.setCurrentRadio(10);
@@ -41,4 +41,72 @@ class RadioTest {
         Assertions.assertEquals(expected, actual);
 
     }
+
+    @Test
+    void setCurrentRadioUndoZero() {
+        Radio station = new Radio();
+
+        station.setCurrentRadio(-1);
+
+        int expected = 0;
+        int actual = station.getCurrentRadio();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void nextRadioAfterNineStation() {
+        Radio station = new Radio();
+        station.setCurrentRadio(9);
+
+        station.next();
+
+        int expected = 0;
+        int actual = station.getCurrentRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void nextRadioUndoNineStation() {
+        Radio station = new Radio();
+        station.setCurrentRadio(8);
+
+        station.next();
+
+        int expected = 9;
+        int actual = station.getCurrentRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void prevRadioAfterZeroStation() {
+        Radio station = new Radio();
+        station.setCurrentRadio(1);
+
+        station.prev();
+
+        int expected = 0;
+        int actual = station.getCurrentRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void prevRadioUndoZeroStation() {
+        Radio station = new Radio();
+        station.setCurrentRadio(0);
+
+        station.prev();
+
+        int expected = 9;
+        int actual = station.getCurrentRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+
 }
